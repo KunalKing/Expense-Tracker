1f966c6dadbcc87705d847c3a62e543f7ef9b0ad
KunalKing <kunalprasad957@gmail.com>
2023-06-16 20:54:23 +0530

    Written Filter code for Expense Tracker app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,29 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import ExpensesList from "./ExpensesList";
+
 
 const Expenses = (props) =>{
 const [filteredYear, setFilteredYear] = useState('2020');
+
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
 
-   return (<div>
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
+
+ 
+   return (<li>
     <Card className="expenses">
     <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-    
-    {/* code for dynamic output of data from app to expenses 27-05-2023 */}
-    {props.items.map((expenses) => (
-      <ExpenseItem 
-       title = {expenses.title}
-       amount = {expenses.amount}
-       date = {expenses.date}
-      />
-    )) }
-    {/* end code */}
-    
+    <ExpensesList items={filteredExpenses}/>
    </Card>
-   </div>
+   </li>
    );
 }
 
